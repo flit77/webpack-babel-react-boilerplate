d6ae40d88fbf7ec0b5c31c487dbd3a111a7959fd
Vasiliy Shubko <flit77@gmail.com>
2017-10-22 23:06:30 +0200

    typo fixed

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 
-import '../css/style.css';
+import './../css/style.css';
 
 export default class Hello extends Component {
   render() {
